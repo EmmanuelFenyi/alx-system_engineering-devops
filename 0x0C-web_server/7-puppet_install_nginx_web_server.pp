244e29148ffa2a2743fcdd7a90ddd538c9107d20
Emmanuel Fenyi <kwamefemma@gmail.com>
2023-09-21 20:50:29 +0000

    load-bal. , web-server, https-ssl

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,23 @@
-# Setup New Ubuntu server with nginx
-
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
-
+# Puppet manifest to install nginx
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+  ensure => installed,
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+# Nginx config
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Nginx config
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+# Nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
