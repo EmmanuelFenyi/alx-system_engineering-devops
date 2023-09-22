5191b9eb0af9c590b281e5c9bfdc349c5c42f26d
Emmanuel Fenyi <kwamefemma@gmail.com>
2023-09-22 20:55:18 +0000

    pupper update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,14 @@
 # Setting up my client config file
-include stdlib
 
 file_line { 'Turn off passwd auth':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Delare identity file':
-  ensure => present,
+file_line { 'add path to find the keys':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '     IdentityFile ~/.ssh/school',
   replace => true,
